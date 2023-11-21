44ce9d6a8c70fdf0f90c9d37c2d38e32a47494a3
Tim K <timk@xfen.page>
2023-11-20 21:52:31 -0800

    JSON output support for lsuhwi

--- lsuhwi.c
+++ lsuhwi.c
@@ -8,8 +8,53 @@ int show_usage(const char* argv0) {
     return 1;
 }
 
+#define UHWI_DEV_TYPE_TO_CSTR(type) \
+    ((type == UHWI_DEV_USB) ? "USB" : "PCI")
+
+void format_as_json(uhwi_dev* current, FILE* where) {
+    if (!current || current->type == UHWI_DEV_NULL)
+        return; // impossible though
+
+    fprintf(where, "{");
+    fprintf(where, "\"type\":\"%s\",\"vendor\":%u,\"device\":%u,",
+                   UHWI_DEV_TYPE_TO_CSTR(current->type),
+                   current->vendor, current->device);
+    fprintf(where, "\"subvendor\":%u,\"subdevice\":%u,",
+                   current->subvendor, current->subdevice);
+    fprintf(where, "\"name\":\"");
+
+    size_t index = 0;
+
+    while (1) {
+        const char cc = current->name[index];
+
+        if (cc == '\0')
+            break;
+
+        switch (cc) {
+            case '"':
+            case '\\': {
+                fputc('\\', where);
+                break;
+            }
+
+            default:
+                break;
+        }
+
+        fputc(cc, where);
+        index++;
+    }
+
+    fprintf(where, "\"}");
+
+    if (current->next)
+        fputc(',', where);
+}
+
 int main(const int argc, const char** argv) {
     uhwi_dev_t type = UHWI_DEV_NULL;
+    size_t as_json = 0;
 
     if (argc >= 2) {
         if (argv[1][0] == '-' && argv[1][1] != '\0') {
@@ -20,6 +65,10 @@ int main(const int argc, const char** argv) {
                 }
                 case 'l': {
                     type = UHWI_DEV_PCI;
+                    break;
+                }
+                case 'J': {
+                    as_json = 1;
                     break;
                 }
                 default:
@@ -35,28 +84,38 @@ int main(const int argc, const char** argv) {
         return 1;
     }
 
+    if (as_json)
+        fputc('[', stdout);
+
     while (first) {
         uhwi_dev* next = first->next;
 
-        if (type == UHWI_DEV_NULL)
-            fprintf(stdout, "[%s] ", (first->type == UHWI_DEV_USB) ? "USB" :
+        if (as_json)
+            format_as_json(first, stdout);
+        else {
+            if (type == UHWI_DEV_NULL)
+                fprintf(stdout, "[%s] ", (first->type == UHWI_DEV_USB) ? "USB" :
                                                                      "PCI");
 
-        fprintf(stdout, "vendor=0x%04x, device=0x%04x", first->vendor,
+            fprintf(stdout, "vendor=0x%04x, device=0x%04x", first->vendor,
                                                         first->device);
 
-        if (first->type == UHWI_DEV_PCI)
-            fprintf(stdout, ", subvendor=0x%04x, subdevice=0x%04x",
-                            first->subvendor, first->subdevice);
+            if (first->type == UHWI_DEV_PCI)
+                fprintf(stdout, ", subvendor=0x%04x, subdevice=0x%04x",
+                                first->subvendor, first->subdevice);
 
-        if (first->name[0] != '\0')
-            fprintf(stdout, ", name: %s", first->name);
+            if (first->name[0] != '\0')
+                fprintf(stdout, ", name: %s", first->name);
 
-        fprintf(stdout, "%c", '\n');
+            fprintf(stdout, "%c", '\n');
+        }
+
         free(first);
-
         first = next;
     }
+
+    if (as_json)
+        fputc(']', stdout);
 
     return 0;
 }
